f1b85d2eed942508ee8b8301e9092eb188430ea1
theodoremoreland <theodoremoreland@yahoo.com>
2023-06-15 04:11:01 -0500

    solved rust/proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {list:?}")
+    let mut proverb: String = String::new();
+
+    for index in 0..list.len() {
+        if index == list.len() - 1 {
+            break;
+        }
+
+        proverb = proverb + &format!("For want of a {} the {} was lost.\n", list[index], list[index + 1]);
+    }
+
+    proverb = if list.len() > 0 { proverb + &format!("And all for the want of a {}.", list[0]) } else { proverb };
+
+    return proverb
 }
