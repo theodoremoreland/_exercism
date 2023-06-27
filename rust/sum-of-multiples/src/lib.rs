77de8e98b9e49cbdf493e78ec7dd5e1b2a10708b
theodoremoreland <theodoremoreland@yahoo.com>
2023-06-27 01:15:42 -0500

    solved rust/sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut set: HashSet<u32> = HashSet::new();
+
+    for factor in factors {
+        let mut multiplier: u32 = 1;
+
+        while (factor > &0) && (factor * multiplier) < limit {
+            
+            set.insert(factor * multiplier);
+
+            multiplier = multiplier + 1;
+        }
+    }
+
+    set.iter().sum()
 }
