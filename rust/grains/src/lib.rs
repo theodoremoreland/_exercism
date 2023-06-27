4cb23fe0209bc964bcdcf31c9f68451787cda066
theodoremoreland <theodoremoreland@yahoo.com>
2023-06-27 10:11:40 -0500

    added solution for rust/grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,21 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {s}");
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+
+    if s == 1 {
+        return 1;
+    }
+
+    u64::pow(2, s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let mut sum: u64 = 1;
+
+    for i in 1..64 {
+        sum += u64::pow(2, i);
+    }
+
+    sum
 }
