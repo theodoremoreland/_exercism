4316033e1c4418dd6510129aa7c069f9181619d3
theodoremoreland <theodoremoreland@yahoo.com>
2023-06-28 02:01:08 -0500

    solved rust/series

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!("What are the series of length {len} in string {digits:?}")
+    let mut sub:  Vec<String> = vec![];
+    let digit_count: usize = digits.len();
+
+    for i in 0..digit_count + 1 {
+        if i + len > digit_count {
+            break;
+        }
+
+        if len == 0 {
+            sub.push(String::new());
+
+            continue;
+        }
+
+        sub.push(digits[i..(i + len)].to_string());
+    }
+
+    sub
 }
