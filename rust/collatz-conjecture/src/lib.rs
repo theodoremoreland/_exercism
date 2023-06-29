b3f56a20d958bb53c27b629a1563b6ccf0a433d5
theodoremoreland <theodoremoreland@yahoo.com>
2023-06-28 21:58:49 -0500

    solved rust/collatz-conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,5 +1,38 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+    let mut steps: Option<u64> = Some(0);
+    let mut current: u64 = n.clone();
+
+    if current == 0 {
+        return None;
+    }
+
+    while current != 1 {
+        if current % 2 == 0 {
+            let quotient: Option<u64> = current.checked_div(2);
+
+            if quotient.is_some() {
+                current = quotient.unwrap();
+            } else {
+                return None;
+            }
+        } else {
+            let product: Option<u64> = current.checked_mul(3);
+
+            if product.is_some() {
+                let sum: Option<u64> = product.unwrap().checked_add(1);
+
+                if sum.is_some() {
+                    current = sum.unwrap();
+                } else {
+                    return None;
+                }
+            } else {
+                return None;
+            }
+        }
+
+        steps = Some(steps.unwrap() + 1);
+    }
+
+    steps
 }
